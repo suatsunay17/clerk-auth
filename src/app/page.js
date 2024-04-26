508f7828a8d7c3be900830b4f5dad9e07313a7d6
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-04-26 14:26:53 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="flex justify-center">
-      <h1 className="text-emerald-600 text-5xl text-center mt-11">Hello, This is Home Page</h1>
+      <h1 className="text-emerald-600 text-5xl text-center mt-11">
+        Hello, This is Home Page
+      </h1>
     </div>
   );
 }
